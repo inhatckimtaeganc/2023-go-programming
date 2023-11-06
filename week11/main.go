5334bf8c5dd3cdefceb8a1c3865f546984568a3a
inhatckimtaeganc <inhatc.kimtaegan.c@gmail.com>
2023-11-06 10:51:53 +0900

    week11 v2.5

--- week11/main.go
+++ week11/main.go
@@ -6,9 +6,14 @@ func main() {
 	primes := [3]int{2, 3, 5} // initialize by array literal
 	//primes[2] = 6
 	for i := 0; i < 3; i++ {
-		fmt.Println(primes[i])
+		fmt.Println(i, primes[i])
 	}
 	// 초기화 하지 않은 원소의 제로 값은 해당 원소 타입의 제로값으로 결정된다
 	test := [5]bool{true, true, true}
 	fmt.Println(test[3]) // zero value
+	fmt.Println(test)
+	//fmt.Println(test[5]) // 컴파일에러, invalid argument: index 5 out of bounds [0:5]
+
+	fmt.Printf("%#v\n", primes)
+	fmt.Printf("%#v\n", test)
 }
