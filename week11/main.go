1848e4dd1c15e3d19fb53c48637209e18a7eac17
inhatckimtaeganc <inhatc.kimtaegan.c@gmail.com>
2023-11-06 11:17:01 +0900

    week11 v2.8

--- week11/main.go
+++ week11/main.go
@@ -21,7 +21,16 @@ func main() {
 		i++
 	}
 
-	for idx, prime := range primes {
-		fmt.Println(idx, prime)
+	// for prime := range primes {  // index만 출력
+	// 	fmt.Println(prime)
+	// }
+
+	// for idx, prime := range primes {  // idx를 사용하지 않아 컴파일 에러
+	// 	fmt.Println(prime)
+	// }
+
+	for _, prime := range primes {
+		fmt.Println(prime)
 	}
+
 }
