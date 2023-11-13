cb278e3bba108f75747319b7e5b7c5b38a137a01
inhatckimtaeganc <inhatc.kimtaegan.c@gmail.com>
2023-11-13 12:00:55 +0900

    week12 v3.6

--- week12/main.go
+++ week12/main.go
@@ -4,13 +4,15 @@ import "fmt"
 
 func main() {
 	a := make([]string, 4, 5)
-	//a := []string{"a", "b", "c", "d"}
 	a[0] = "a"
 	a[2] = "c"
 	a[3] = "d"
-	as := a[0:2] // ["a", ""]
+	as := a[0:2]
+	//c := append(a, "y") // 메모리공간이 늘어나지 않는다
+	c := append(a, "y", "x") // 메모리 공간을 늘린다
 	fmt.Println(a, len(a), cap(a))
-	as[1] = "z" // ["a", "z"]
-	fmt.Println(a, len(a), cap(a))
-	fmt.Printf("%x %x %x\n", &a[0], &as[0], &a[1])
+	fmt.Println(c, len(c), cap(c))
+	as[1] = "z"
+	// fmt.Println(a, len(a), cap(a))
+	// fmt.Printf("%x %x %x\n", &a[0], &as[0], &a[1])
 }
