d06b9c8181f9e7117c3c14c2637ca9aea3007c4f
inhatckimtaeganc <inhatc.kimtaegan.c@gmail.com>
2023-11-27 11:32:32 +0900

    week14 v4.6

--- week14/main.go
+++ week14/main.go
@@ -3,22 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	games := map[int]string{
-		456: "성기훈",
-		218: "박해수",
-		067: "강새벽",
-		001: "오일남",
-		199: "알리",
-		101: "아이오아이",
-	}
-
-	for _, v := range games {
-		fmt.Println(v)
-	}
-	games[101] = "장덕수" // update
-	delete(games, 199) // delete
-
-	for k, v := range games {
-		fmt.Println(k, v)
-	}
+	//balls := make(map[string]int)
+	var balls map[string]int
+	fmt.Printf("%#v\n", balls)
+	balls["성기훈"] = 20
+	fmt.Println(balls["성기훈"])
+	fmt.Println(balls["오일남"])
 }
