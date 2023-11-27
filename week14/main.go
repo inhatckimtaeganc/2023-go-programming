4c65d9f7766ace0022974ddbc5bfb77ef60c95fc
inhatckimtaeganc <inhatc.kimtaegan.c@gmail.com>
2023-11-27 11:36:23 +0900

    week14 v4.7

--- week14/main.go
+++ week14/main.go
@@ -2,11 +2,14 @@ package main
 
 import "fmt"
 
+func status(name string) {
+	balls := map[string]int{"성기훈": 20, "오일남": 0}
+	//ball := balls[name]
+	ball, exists := balls[name]
+	fmt.Println(ball, exists)
+}
 func main() {
-	//balls := make(map[string]int)
-	var balls map[string]int
-	fmt.Printf("%#v\n", balls)
-	balls["성기훈"] = 20
-	fmt.Println(balls["성기훈"])
-	fmt.Println(balls["오일남"])
+	status("오일남")
+	status("강철")
+	status("성기훈")
 }
